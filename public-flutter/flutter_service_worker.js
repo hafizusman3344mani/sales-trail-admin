d64f065ba613e475bd02324f318ff8bd251af1bf
usman <hafizusman3344mani@gmail.com>
2024-05-31 00:15:12 +0500

    changed

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4bb0794ee921fb968beb9c78c53e2a7e",
+const RESOURCES = {"flutter_bootstrap.js": "4a86318c6078d47995f399db957aafec",
 "version.json": "649eaebb15c281ab61f973f3997cc402",
 "index.html": "2a00b88f039db2617fde86ab859de6aa",
 "/": "2a00b88f039db2617fde86ab859de6aa",
-"main.dart.js": "ed371eb304d39a992b5015b0576ddb90",
+"main.dart.js": "0e7e7069f2995eb62a565d046a4f8313",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
